538a6337cce788fc177c4a59aba8a9cb0160d966
Nelson Alexander <AlexanderNelson@tfl.gov.uk>
2018-08-21 16:02:58 +0100

    Added UI features

--- src/Components/InDepth.js
+++ src/Components/InDepth.js
@@ -6,9 +6,46 @@ class InDepth extends Component {
 
     render() {
         return (
-            <div>
-                In Depth
-                </div>
+            <div class="card-columns">
+                <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Article 1</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+  </div>
+                    <div class="card">
+                            <div class="card-body">
+                        <h5 class="card-title">Article 2</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+  </div>
+                        
+                        <div class="card text-center">
+                            <div class="card-body">
+                        <h5 class="card-title">Article 3</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                            <div class="card p-3 text-right">
+                                <blockquote class="blockquote mb-0">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                                    <footer class="blockquote-footer">
+                                        <small class="text-muted">
+                                            Someone famous in <cite title="Source Title">Source Title</cite>
+                                        </small>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                        <h5 class="card-title">Article 4</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
         );
     }
 }
--- src/Persistent Components/NavHead.js
+++ src/Persistent Components/NavHead.js
@@ -4,14 +4,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import '../bootstrap.css';
+import '../App.css';
 import { Link } from 'react-router-component';
 
 class NavHead extends Component {
     render() {
         return (
             <div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light NavHead">
+    <a class="navbar-brand" href="#">Website</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,24 +20,21 @@ class NavHead extends Component {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                                <Link class="nav-link" href="/InDepth">Home</Link>
+                                <Link class="nav-link" href="/">Home</Link>
                             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="/InDepth">InDepth</a>
                             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item dropdown active">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown
         </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                   <div class="dropdown-menu" onClick={this.toggleOpen} aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
             </li>
         </ul>
   </div>

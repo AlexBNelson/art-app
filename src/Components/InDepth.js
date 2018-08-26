31b454a867f9d0427fcb0c414c68653dc1ef40a3
Nelson Alexander <AlexanderNelson@tfl.gov.uk>
2018-08-26 10:38:43 +0100

    Changes to indepth pages for ui

--- src/Components/InDepth.js
+++ src/Components/InDepth.js
@@ -1,5 +1,6 @@
 import { Location, Locations } from 'react-router-component';
 import React, { Component } from 'react';
+import '../bootstrap.css';
 
 
 class InDepth extends Component {
@@ -7,8 +8,48 @@ class InDepth extends Component {
     render() {
         return (
             <div>
-                In Depth
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-sm-3" style={{ margin: 10 }}>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-7">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+
         );
     }
 }
--- src/Persistent Components/NavHead.js
+++ src/Persistent Components/NavHead.js
@@ -19,10 +19,10 @@ class NavHead extends Component {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                                <Link class="nav-link" href="/InDepth">Home</Link>
+                                <Link class="nav-link" href="/">Home</Link>
                             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                                <Link class="nav-link" href="/InDepth">InDepth</Link>
                             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
